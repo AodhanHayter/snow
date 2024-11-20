cf98fe262734968d2eabb65ebf5b5b3d76c62984
Aodhan Hayter <aodhan.hayter@gmail.com>
2024-11-20 15:14:43 -0700

    setting up gluster for k3s cluster

--- modules/nixos/services/kubernetes/default.nix
+++ modules/nixos/services/kubernetes/default.nix
@@ -8,14 +8,21 @@ with lib.modernage; let
   cfg = config.modernage.services.kubernetes;
 in
 {
-  options.modernage.kubernetes = with types; {
+  options.modernage.services.kubernetes = with types; {
     enable = mkBoolOpt false "Whether or not to enable kubernetes configuration";
     role = mkOpt (enum [ "master" "node" ]) "node" "The role this kubernetes machine should take";
+    masterAddress = mkOpt (nullOr str) null "The clusterwide available network address or hostname for the kubernetes master server.";
+    clusterCidr = mkOpt (nullOr str) null "CIDR range for pods in cluster";
   };
 
   config = mkIf cfg.enable {
     services.kubernetes = {
-      role = [ cfg.role ];
+      roles = [ cfg.role ];
+      easyCerts = true;
+      masterAddress =
+        if cfg.role == "master"
+        then cfg.masterAddress else throw "If role is set to 'master' then a masterAddress must be configured.";
+      clusterCidr = cfg.clusterCidr;
     };
   };
 }
