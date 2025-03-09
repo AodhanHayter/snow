aad282b34fd77d30d07ad07ca63280c4cf292d06
Aodhan Hayter <aodhan.hayter@gmail.com>
2025-03-09 13:49:29 -0600

    add tealdeer cli tool

---
+++ modules/home/cli-apps/tealdeer/default.nix
@@ -0,0 +1,18 @@
+{ lib, config, ... }:
+with lib;
+with lib.modernage;
+let
+  cfg = config.modernage.cli-apps.tealdeer;
+in
+{
+  options.modernage.cli-apps.tealdeer = {
+    enable = mkBoolOpt false "Whether or not to install and configure tealdeer.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.tealdeer = {
+      enable = true;
+      enableAutoUpdates = true;
+    };
+  };
+}
