df214e919ebe022f253f6c3f993e24fbe89e1dea
Aodhan Hayter <aodhan.hayter@gmail.com>
2025-07-12 22:18:37 -0600

    add macmini SSH public key to lab-nodes openssh configuration

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -10,13 +10,14 @@ with lib.modernage; let
   cfg = config.modernage.services.openssh;
   default-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFnEsBv5zrOZzeQSymd/WKottg28l0mav/J0m0/Q3E4X aodhan.hayter@gmail.com";
   desktop-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMTBSBlivmm4W46rP9m+qHPwumFuepcjP9Jl6iYhcZS5 aodhan.hayter@gmail.com";
+  macmini-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL5Gq86apfFdEbHIvTK+n1f7txgRYDakWfTARSzct0om aodhan.hayter@gmail.com";
 in
 {
   options.modernage.services.openssh = with types;
     {
       enable =
         mkBoolOpt false "Whether or not to enable openssh configuration.";
-      authorizedKeys = mkOpt (listOf str) [ default-key desktop-key ] "The public keys to apply.";
+      authorizedKeys = mkOpt (listOf str) [ default-key desktop-key macmini-key ] "The public keys to apply.";
       port = mkOpt port 2222 "The port to listen on (in addition to 22).";
     };
 
