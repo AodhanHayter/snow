d7bddc66e46973ea0c6e2ccabb01911e8985d811
Aodhan Hayter <aodhan.hayter@gmail.com>
2025-12-26 22:48:57 -0700

    update beads overlay

--- overlays/beads/default.nix
+++ overlays/beads/default.nix
@@ -1,4 +1,6 @@
 { inputs, ... }:
 final: prev: {
-  beads = inputs.beads.packages.${prev.stdenv.hostPlatform.system}.default;
+  beads = inputs.beads.packages.${prev.stdenv.hostPlatform.system}.default.overrideAttrs (old: {
+    vendorHash = "sha256-ovG0EWQFtifHF5leEQTFvTjGvc+yiAjpAaqaV0OklgE=";
+  });
 }
