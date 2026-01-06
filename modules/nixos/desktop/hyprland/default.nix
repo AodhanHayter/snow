e46e31d28b38a16892c61314b0bd4b930483884a
Aodhan Hayter <aodhan.hayter@gmail.com>
2026-01-05 23:03:06 -0700

    update hyprland conf

--- modules/nixos/desktop/hyprland/default.nix
+++ modules/nixos/desktop/hyprland/default.nix
@@ -51,6 +51,7 @@ in
     };
 
     environment.systemPackages = with pkgs; [
+      adwaita-icon-theme # traditional cursor theme
       bluetui # Bluetooth management tool
       btop # system resource monitor
       dunst # lightweight notification daemon
