2c82e2839954f2e3c0ebd384ff0001bc239e24c3
Aodhan Hayter <aodhan.hayter@gmail.com>
2026-03-28 22:43:49 -0600

    darwin: use native nix-darwin openssh.authorizedKeys
    nix-darwin now supports users.users.<name>.openssh.authorizedKeys.keys
    directly, no need for custom activation scripts or extraConfig.

--- modules/darwin/security/ssh/default.nix
+++ modules/darwin/security/ssh/default.nix
@@ -9,8 +9,6 @@ with lib;
 with lib.modernage;
 let
   cfg = config.modernage.security.ssh;
-
-  authorizedKeysText = concatStringsSep "\n" cfg.authorizedKeys + "\n";
 in
 {
   options.modernage.security.ssh = with types; {
@@ -19,28 +17,15 @@ in
     authorizedKeys = mkOpt (listOf str) [ ] "Public keys to authorize for the primary user.";
   };
 
-  config = let
-    user = config.modernage.user.name;
-    authorizedKeysDir = "/etc/ssh/authorized_keys.d";
-    authorizedKeysPath = "${authorizedKeysDir}/${user}";
-    authorizedKeysSrc = pkgs.writeText "authorized_keys-${user}" authorizedKeysText;
-  in mkMerge [
+  config = mkMerge [
     (mkIf cfg.enable {
       environment.systemPackages = with pkgs; [ openssh ];
     })
     (mkIf cfg.server {
-      services.openssh = {
-        enable = true;
-        extraConfig = ''
-          AuthorizedKeysFile ${authorizedKeysDir}/%u
-        '';
-      };
+      services.openssh.enable = true;
     })
     (mkIf (cfg.authorizedKeys != [ ]) {
-      system.activationScripts.postActivation.text = ''
-        mkdir -p ${authorizedKeysDir}
-        install -m 644 -o ${user} ${authorizedKeysSrc} ${authorizedKeysPath}
-      '';
+      users.users.${config.modernage.user.name}.openssh.authorizedKeys.keys = cfg.authorizedKeys;
     })
   ];
 }
