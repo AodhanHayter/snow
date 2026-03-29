09ea7eb63b67ba089a187d210ff8d826f239f366
Aodhan Hayter <aodhan.hayter@gmail.com>
2026-03-28 20:32:36 -0600

    darwin: manage ssh authorized keys via /etc/ssh like NixOS
    Write keys to /etc/ssh/authorized_keys.d/<user> and configure
    sshd AuthorizedKeysFile to read from there. Removes home-manager
    symlink approach that macOS sshd rejected.

--- modules/darwin/security/ssh/default.nix
+++ modules/darwin/security/ssh/default.nix
@@ -9,11 +9,14 @@ with lib;
 with lib.modernage;
 let
   cfg = config.modernage.security.ssh;
+
+  authorizedKeysText = concatStringsSep "\n" cfg.authorizedKeys + "\n";
 in
 {
   options.modernage.security.ssh = with types; {
     enable = mkBoolOpt false "Whether or not to install openssh"; # macOS now ships with FIDO2/yubikey support; Nix openssh causes firewall issues
     server = mkBoolOpt false "Whether to enable Remote Login (SSH server).";
+    authorizedKeys = mkOpt (listOf str) [ ] "Public keys to authorize for the primary user.";
   };
 
   config = mkMerge [
@@ -21,7 +24,15 @@ in
       environment.systemPackages = with pkgs; [ openssh ];
     })
     (mkIf cfg.server {
-      services.openssh.enable = true;
+      services.openssh = {
+        enable = true;
+        extraConfig = ''
+          AuthorizedKeysFile /etc/ssh/authorized_keys.d/%u
+        '';
+      };
+    })
+    (mkIf (cfg.authorizedKeys != [ ]) {
+      environment.etc."ssh/authorized_keys.d/${config.modernage.user.name}".text = authorizedKeysText;
     })
   ];
 }
