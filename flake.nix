173754cc93bab17e8e99493129826091961cf8ba
Aodhan Hayter <aodhan.hayter@gmail.com>
2026-04-17 14:21:40 -0600

    macos: update flake inputs

--- flake.nix
+++ flake.nix
@@ -95,6 +95,11 @@
       flake = false;
     };
 
+    caveman = {
+      url = "github:JuliusBrussee/caveman";
+      flake = false;
+    };
+
     berkeley-mono = {
       url = "git+ssh://git@github.com/AodhanHayter/berkeley-mono-nix?ref=master";
       inputs.nixpkgs.follows = "nixpkgs";
